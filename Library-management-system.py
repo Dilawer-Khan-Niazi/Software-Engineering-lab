d3ebd5b7efc6112b9aafb3c61c20a99cbae98b59
DaniyalSaffi <daniyals0316@gmail.com>
2023-10-22 21:09:33 +0500

    Dani added something

--- Library-management-system.py
+++ Library-management-system.py
@@ -50,7 +50,6 @@ def login_user(username, password):
     workbook.close()
     return "Login failed. Please check your username and password."
 
-# Function to reset a user's password
 def reset_password(username, new_password):
     create_excel_file()
     workbook = openpyxl.load_workbook('library_data.xlsx')
@@ -61,10 +60,13 @@ def reset_password(username, new_password):
             user_sheet.cell(row=row_index, column=2, value=new_password)
             workbook.save('library_data.xlsx')
             workbook.close()
-            return "Password reset successful."
+            return "Password reset successfully."
 
     workbook.close()
     return "User not found. Please check the username."
+
+    workbook.close()
+    return "User not found. Please check the usernamece."
 
 # Function to add a book to the catalog
 def add_book(title, author):
@@ -100,6 +102,45 @@ def delete_book(title, author):
 
     workbook.close()
     return "Book not found in the catalog."
+
+def search_books():
+    create_excel_file()
+    workbook = openpyxl.load_workbook('library_data.xlsx')
+    books_sheet = workbook['Books']
+
+    while True:
+        search_query = input("Enter a book title or author (or type 'q' to quit): ").strip()
+
+        if search_query.lower() == 'q':
+            break
+
+        found_books = []
+
+        for row in books_sheet.iter_rows(min_row=2, values_only=True):
+            title, _, author, _ = row  # Adjust this line to match the row structure
+            if search_query.lower() in title.lower() or search_query.lower() in author.lower():
+                found_books.append((title, author))
+
+        if found_books:
+            print("Matching Books:")
+            for title, author in found_books:
+                print(f"Title: {title}, Author: {author}")
+        else:
+            print("No matching books found.")
+
+    workbook.close()
+
+def display_catalog():
+    create_excel_file()
+    workbook = openpyxl.load_workbook('library_data.xlsx')
+    books_sheet = workbook['Books']
+
+    print("Catalog of Books:")
+    for row in books_sheet.iter_rows(min_row=2, values_only=True):
+        title, _ , author, _  = row  
+        print(f"Title: {title}, Author: {author}")
+
+    workbook.close()
 
 # Function to issue a book to a member
 def issue_book(username, title):
@@ -180,53 +221,8 @@ def return_book(username, title):
 
     workbook.close()
     return f"{title} is not in your possession or not found in the catalog."
-# Function to search for books by title or author
-# Function to search for books by title or author
-def search_books():
-    create_excel_file()
-    workbook = openpyxl.load_workbook('library_data.xlsx')
-    books_sheet = workbook['Books']
-
-    while True:
-        search_query = input("Enter a book title or author (or type 'q' to quit): ").strip()
-
-        if search_query.lower() == 'q':
-            break
-
-        found_books = []
-
-        for row in books_sheet.iter_rows(min_row=2, values_only=True):
-            title, _, author, _ = row  # Adjust this line to match the row structure
-            if search_query.lower() in title.lower() or search_query.lower() in author.lower():
-                found_books.append((title, author))
-
-        if found_books:
-            print("Matching Books:")
-            for title, author in found_books:
-                print(f"Title: {title}, Author: {author}")
-        else:
-            print("No matching books found.")
-
-    workbook.close()
 
 
-
-
-# Function to display the book catalog
-def display_catalog():
-    create_excel_file()
-    workbook = openpyxl.load_workbook('library_data.xlsx')
-    books_sheet = workbook['Books']
-
-    print("Catalog of Books:")
-    for row in books_sheet.iter_rows(min_row=2, values_only=True):
-        title, _ , author, _  = row  # Adjust this line to match the row structure
-        print(f"Title: {title}, Author: {author}")
-
-    workbook.close()
-
-
-# Main program loop
 while True:
     print("Options:")
     print("1. Register a user")
